715f9323b359bb176cb534e2e763c2c1d2250728
YolianBoister <yolianboister@gmail.com>
2023-11-28 12:03:44 +0000

    Added comments

--- python music game.py
+++ python music game.py
@@ -1,11 +1,12 @@
 print("Welcome to the music quiz game!")
 print("Only these users are allowed to play: Fred, Billy, Jack")
-#Could use an array/list to name the users ^
+#could use an array/list to name the users ^
 
 #make an array/list to verify that the user's input = auth_users
 auth_users = ["Fred", "Billy", "Jack", "fred", "billy", "jack"]
 
-
+#make a function that asks the user for their name and denies
+#and loops until the user inputs a correct user
 def check():
     input_name = input("What is your name?")
     if input_name in auth_users:
@@ -15,4 +16,6 @@ def check():
         print("Unauthorised user try again!")
         check()
 check()
-print("Continued")
+
+f = open("data.txt")
+

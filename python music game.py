6ccb14ee8e6463734c64bf24e5fd6c279c5c3096
YolianBoister <yolianboister@gmail.com>
2023-12-05 11:50:11 +0000

    Added test for reading external files
    Uses lists to read contents of file and put into a list
    Uses lists to number each line that can be called
    Uses comparison between 0 and (number of items in list) to validate input
    Currently broken (line_func gets broken after first use)

--- python music game.py
+++ python music game.py
@@ -4,7 +4,6 @@ print("Only these users are allowed to play: Fred, Billy, Jack")
 
 #make an array/list to verify that the user's input = auth_users
 auth_users = ["Fred", "Billy", "Jack", "fred", "billy", "jack"]
-
 #make a function that asks the user for their name and denies
 #and loops until the user inputs a correct user
 def check():
@@ -15,7 +14,25 @@ def check():
     else:
         print("Unauthorised user try again!")
         check()
-check()
 
+check()
 f = open("data.txt")
 
+def line_func():
+    input_line = 0
+    content = f.readlines()
+    valid_range = False
+    list_size = len(content)
+    print(list_size)
+    while not valid_range:
+        try:
+            input_line = int(input("Choose line"))
+            if input_line >= 0 and input_line <= list_size:
+                valid_range = True
+        except ValueError:
+            print("Not a valid line number")
+            
+    input_line = input_line - 1
+    print(content[input_line])
+    line_func()
+line_func()
